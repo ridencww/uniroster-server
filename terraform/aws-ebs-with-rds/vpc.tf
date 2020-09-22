f7565b1b5bbc4c894a9b05221dd66bfc027ab076
Mike Fugate <mfugate@carnegielearning.com>
2020-09-22 15:38:07 -0400

    use private subnet for nat

--- terraform/aws-ebs-with-rds/vpc.tf
+++ terraform/aws-ebs-with-rds/vpc.tf
@@ -143,7 +143,7 @@ resource "aws_eip" "nat" {
 
 resource "aws_nat_gateway" "nat-gw" {
   allocation_id = aws_eip.nat.id
-  subnet_id     = aws_subnet.main-public-1.id
+  subnet_id     = aws_subnet.main-private-1.id
   depends_on    = [aws_internet_gateway.main-gw]
 }
 
