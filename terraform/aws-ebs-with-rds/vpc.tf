5b67d80a9a868dd8e78a134f8d55a3314cfb2ac0
Mike Fugate <mfugate@carnegielearning.com>
2020-09-22 15:37:06 -0400

    need the private route table and nat

--- terraform/aws-ebs-with-rds/vpc.tf
+++ terraform/aws-ebs-with-rds/vpc.tf
@@ -112,13 +112,13 @@ resource "aws_internet_gateway" "main-gw" {
 #  route_table_id = aws_route_table.main-public.id
 #}
 #
-#resource "aws_route_table" "main-private" {
-#  vpc_id = aws_vpc.main.id
-#  route {
-#    cidr_block     = "0.0.0.0/0"
-#    nat_gateway_id = aws_nat_gateway.nat-gw.id
-#  }
-#}
+resource "aws_route_table" "main-private" {
+  vpc_id = aws_vpc.main.id
+  route {
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.nat-gw.id
+  }
+}
 
 # route associations private
 resource "aws_route_table_association" "main-private-1-a" {
@@ -141,11 +141,11 @@ resource "aws_eip" "nat" {
   vpc = true
 }
 
-#resource "aws_nat_gateway" "nat-gw" {
-#  allocation_id = aws_eip.nat.id
-#  subnet_id     = aws_subnet.main-public-1.id
-#  depends_on    = [aws_internet_gateway.main-gw]
-#}
+resource "aws_nat_gateway" "nat-gw" {
+  allocation_id = aws_eip.nat.id
+  subnet_id     = aws_subnet.main-public-1.id
+  depends_on    = [aws_internet_gateway.main-gw]
+}
 
 resource "aws_ec2_transit_gateway_vpc_attachment" "transit-gateway" {
   subnet_ids         = [aws_subnet.main-private-1.id, aws_subnet.main-private-2.id, aws_subnet.main-private-3.id]
