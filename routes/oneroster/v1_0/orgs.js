cd7f0d3e3e04edcfa524648b8db3ab08a796b94d
Mike Fugate <mfugate@carnegielearning.com>
2020-10-05 12:52:55 -0400

    refactor orgs routes to use db promises and new authentication

--- routes/oneroster/v1_0/orgs.js
+++ routes/oneroster/v1_0/orgs.js
@@ -1,157 +1,86 @@
-var app = require('../../../uniroster-server.js');
-var db = require('../../../lib/database.js');
-var express = require('express');
-var utils = require('../../../lib/onerosterUtils.js');
+const db = require('../../../utils/database');
+const router = require('express').Router();
+const table = 'orgs';
+const utils = require('../../../utils/utils');
 
-var router = express.Router();
-
-var buildOrg = function(row, hrefBase, metaFields) {
-  var org = {};
-  org.sourcedId = row.sourcedId;
-  org.status = row.status ? row.status : "active";
-  org.dateLastModified = row.dateLastModified;
-
-  var metadata = {};
-  metaFields.forEach(function(field) {
-    metadata[field.jsonColumn] = row[field.dbColumn];
-  });
-  if (metaFields.length > 0) {
-    org.metadata = metadata;
-  }
-
-  org.name = row.name;
-  org.type = row.type;
-  org.identifier = row.identifier;
-
-  if (row.parentSourcedId) {
-    var parent = {};
-    parent.href = hrefBase + '/orgs/' + row.parentSourcedId;
-    parent.sourcedId = row.parentSourcedId;
-    parent.type = 'org';
-    org.parent = parent;
-  }
-
-  if (row.childSourcedIds) {
-    var children = [];
-    var fields = row.childSourcedIds.toString().split(",");
-    fields.forEach(function(sid) {
-      var child = {};
-      child.href = hrefBase + '/orgs/' + sid;
-      child.sourcedId = sid;
-      child.type = 'org';
-      children.push(child);
+function buildOrg(row, hrefBase, metaFields) {
+    const org = {};
+    org.sourcedId = row.sourcedId;
+    org.status = row.status ? row.status : "active";
+    org.dateLastModified = row.dateLastModified;
+  
+    const metadata = {};
+    metaFields.forEach(function(field) {
+        metadata[field.jsonColumn] = row[field.dbColumn];
     });
-    org.children = children;
-  }
-
-  return org;
-};
-
-var queryOrg = function(req, res, next, type) {
-  db.setup(req, res, function(connection, hrefBase) {
-    db.tableFields(connection, 'orgs', function(fields) {
-      var select = utils.buildSelectStmt(req, res, fields);
-      if (select === null) {
-        connection.release();
-        return;
-      }
-
-      var where = utils.buildWhereStmt(req, res, fields, '', 'sourcedId = ? ' + (type ? ' AND type = ?' : ''));
-      if (where === null) {
-        connection.release();
-        return;
-      }
-
-      var orderBy = utils.buildOrderByStmt(req, res, fields);
-      if (orderBy === null) {
-        connection.release();
-        return;
-      }
-
-      var sql = select + ' FROM orgs ';
-      sql += where;
-      sql += orderBy;
-      sql += ' LIMIT 1';
-
-      connection.query(sql, [req.params.id, type], function(err, rows) {
-        connection.release();
-
-        if (err) {
-          utils.reportServerError(res, err);
-          return;
-        }
-
-        if (rows.length == 0) {
-          utils.reportNotFound(res);
-        } else {
-          var wrapper = {};
-          wrapper.org = buildOrg(rows[0], hrefBase, fields.metaFields);
-          res.json(wrapper);
-        }
-      });
-    });
-  });
-};
-
-var queryOrgs = function(req, res, next, type) {
-  db.setup(req, res, function(connection, hrefBase) {
-    db.tableFields(connection, 'orgs', function(fields) {
-      var select = utils.buildSelectStmt(req, res, fields);
-      if (select === null) {
-        connection.release();
-        return;
-      }
-
-      var where = utils.buildWhereStmt(req, res, fields, '', type ? ' type = ?' : '');
-      if (where === null) {
-        connection.release();
-        return;
-      }
-
-      var orderBy = utils.buildOrderByStmt(req, res, fields);
-      if (orderBy === null) {
-        connection.release();
-        return;
-      }
-
-      var sql = select + ' FROM orgs ';
-      sql += where;
-      sql += orderBy;
-      sql += utils.buildLimitStmt(req);
-
-      connection.query(sql, [type], function(err, rows) {
-        connection.release();
-
-        if (err) {
-          utils.reportServerError(res, err);
-          return;
-        }
-
-        var wrapper = {};
-        wrapper.orgs = [];
-        rows.forEach(function(row) {
-          wrapper.orgs.push(buildOrg(row, hrefBase, fields.metaFields));
+    if (metaFields.length > 0) {
+        org.metadata = metadata;
+    }
+  
+    org.name = row.name;
+    org.type = row.type;
+    org.identifier = row.identifier;
+  
+    if (row.parentSourcedId) {
+        const parent = {};
+        parent.href = hrefBase + '/orgs/' + row.parentSourcedId;
+        parent.sourcedId = row.parentSourcedId;
+        parent.type = 'org';
+        org.parent = parent;
+    }
+  
+    if (row.childSourcedIds) {
+        const children = [];
+        const fields = row.childSourcedIds.toString().split(",");
+        fields.forEach(function(sid) {
+            const child = {};
+            child.href = hrefBase + '/orgs/' + sid;
+            child.sourcedId = sid;
+            child.type = 'org';
+            children.push(child);
         });
+        org.children = children;
+    }
+  
+    return org;
+};
+
+function queryOrg(req, res, next, type) {
+    db.getData(req, res, table, [req.params.id, type], 'sourcedId = ? ' + (type ? ' AND type = ?' : '')).then((data) => {
+        const hrefBase = utils.getHrefBase(req);
+        const wrapper = {
+            orgs: buildOrg(data.results[0], hrefBase, data.fields.metaFields)
+        };
         res.json(wrapper);
-      });
     });
-  });
+};
+
+function queryOrgs(req, res, next, type) {
+    db.getData(req, res, table, [type], type ? ' type = ?' : '').then((data) => {
+        const hrefBase = utils.getHrefBase(req);
+        const wrapper = {};
+        wrapper.orgs = [];
+        data.results.forEach(function(row) {
+            wrapper.orgs.push(buildOrg(row, hrefBase, data.fields.metaFields));
+        })
+        res.json(wrapper);
+    });
 };
 
 router.get('/orgs', function(req, res, next) {
-  queryOrgs(req, res, next);
+    queryOrgs(req, res, next);
 });
-
-router.get('/schools', function(req, res, next) {
-  queryOrgs(req, res, next, "school");
+  
+  router.get('/schools', function(req, res, next) {
+    queryOrgs(req, res, next, "school");
 });
-
+  
 router.get('/orgs/:id', function(req, res, next) {
-  queryOrg(req, res, next);
+    queryOrg(req, res, next);
 });
 
 router.get('/schools/:id', function(req, res, next) {
-  queryOrg(req, res, next, "school");
+    queryOrg(req, res, next, "school");
 });
 
 module.exports = router;
