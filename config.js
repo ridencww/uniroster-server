12c9a6e6b6050d8b45303a55547be088891fc09a
Mike Fugate <mfugate@carnegielearning.com>
2020-10-05 12:51:59 -0400

    add support for environment variables in config

--- config.js
+++ config.js
@@ -1,19 +1,20 @@
 var db = {};
-db.host = 'mariadb';
-db.user = 'root';
-db.password = 'password';
-db.database = 'uniroster';
-db.connectionLimit = 10;
+db.host = process.env.DB_HOSTNAME || 'localhost';
+db.user = process.env.DB_USERNAME || 'root';
+db.password = process.env.DB_PASSWORD || 'password';
+db.database = process.env.DATABASE || 'accounts';
+db.authDatabase = process.env.AUTH_DATABASE || 'accounts';
+db.connectionLimit = process.env.DB_CONNECTION_LIMIT ? parseInt(process.env.DB_CONNECTION_LIMIT, 10) : 10;
 
 var config = {};
 config.db = db;
 
-config.httpActive = true;
-config.httpPort = 3000;
+config.httpActive = process.env.HTTP_ACTIVE ? process.env.HTTP_ACTIVE === "true" : true;
+config.httpPort = process.env.HTTP_PORT ? parseInt(process.env.HTTP_PORT, 10) : 80;
 
-config.httpsActive = false;
-config.httpsPort = 3443;
-config.httpsCert = './bin/cert.pem';
-config.httpsPrivateKey = './bin/key.pem';
+config.httpsActive = process.env.HTTPS_ACTIVE ? process.env.HTTPS_ACTIVE === "true" : true;
+config.httpsPort = process.env.HTTPS_PORT ? parseInt(process.env.HTTPS_PORT, 10) : 443;
+config.httpsCert = process.env.HTTPS_CERT || './certs/server.cert';
+config.httpsPrivateKey = process.env.HTTPS_PRIVATE_KEY || './certs/server.key';
 
 module.exports = config;
