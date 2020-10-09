a190a845fc6d1b88dd0f123e71a1e23bd5fad34a
Mike Fugate <mfugate@carnegielearning.com>
2020-10-09 10:10:08 -0400

    remove unused config parameter

--- config.js
+++ config.js
@@ -2,7 +2,6 @@ var db = {};
 db.host = process.env.DB_HOSTNAME || 'localhost';
 db.user = process.env.DB_USERNAME || 'root';
 db.password = process.env.DB_PASSWORD || 'password';
-db.database = process.env.DATABASE || 'accounts';
 db.authDatabase = process.env.AUTH_DATABASE || 'accounts';
 db.connectionLimit = process.env.DB_CONNECTION_LIMIT ? parseInt(process.env.DB_CONNECTION_LIMIT, 10) : 10;
 
@@ -12,7 +11,7 @@ config.db = db;
 config.httpActive = process.env.HTTP_ACTIVE ? process.env.HTTP_ACTIVE === "true" : true;
 config.httpPort = process.env.HTTP_PORT ? parseInt(process.env.HTTP_PORT, 10) : 80;
 
-config.httpsActive = process.env.HTTPS_ACTIVE ? process.env.HTTPS_ACTIVE === "true" : true;
+config.httpsActive = process.env.HTTPS_ACTIVE ? process.env.HTTPS_ACTIVE === "true" : false;
 config.httpsPort = process.env.HTTPS_PORT ? parseInt(process.env.HTTPS_PORT, 10) : 443;
 config.httpsCert = process.env.HTTPS_CERT || './certs/server.cert';
 config.httpsPrivateKey = process.env.HTTPS_PRIVATE_KEY || './certs/server.key';
